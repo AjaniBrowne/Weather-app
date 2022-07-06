2506b5ab82553796b68f049ba538aed7a7fb06ab
AjaniBrowne <ajani.browne@yahoo.com>
2022-07-06 16:03:45 -0400

    Commit on Main " aded third paty apis"

---
+++ script.js
@@ -0,0 +1,3 @@
+var currentDate = moment().format("MMM Do YY")
+
+fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={a7f5d6a06c65976eea30661bb630099b}');
